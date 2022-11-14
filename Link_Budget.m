eda88c69feba12429c08847c5359b0924a8b915f
Neithiri <leonie.deu@outlook.fr>
2022-11-14 12:52:54 +0100

    Update Link_Budget.m

--- Link_Budget.m
+++ Link_Budget.m
@@ -1,3 +1,6 @@
 %%%%%%%%%%%%%%%%%%%%%%%%% PAYLOAD DESIGN PROJECT %%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%      LINK BUDGET       %%%%%%%%%%%%%%%%%%%%%%%%%
 
+% Setting parameters
+R = 800e3; % detection range of the target
+sigma_0
